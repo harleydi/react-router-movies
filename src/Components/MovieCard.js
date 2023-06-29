df9b519fbae6538a1b636bfa2baeae4369c048cd
harleydi <disneyharley26@gmail.com>
2023-06-29 19:23:54 -0400

    added DELETE functionality and added delete button

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useOutletContext } from 'react-router-dom'
 
 
 
 const MovieCard = ({ movie }) => {
   const navigate = useNavigate()
+  const { movies, setMovies } = useOutletContext()
+
+  const handleDelete = () => {
+    const removeMovie = movies.filter((movieDelete) => movieDelete.id !== movie.id )
+    setMovies(removeMovie)
+    navigate("/")
+  }
+
+
   return (
     <div>
         <h1>{movie.title}</h1>
@@ -12,6 +21,7 @@ const MovieCard = ({ movie }) => {
         <p>{movie.genre}</p>
         <button onClick={() => navigate(`/movie/${movie.id}/details`)}>View</button>
         <button onClick={() => navigate(`/movies/${movie.id}/edit`)}>Edit</button>
+        <button onClick={handleDelete}>Delete</button>
     </div>
   )
 }
